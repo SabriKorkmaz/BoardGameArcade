ca4f7f65ebcd48eb59b75f7bf81b4414aa204b41
Sabri Korkmaz <sabri_51@hotmail.com>
2022-12-10 14:02:09 +0300

    landing page design completed

--- boardBox/src/routes/Landing.tsx
+++ boardBox/src/routes/Landing.tsx
@@ -9,40 +9,36 @@ const Landing = ({navigation, route}: Props) => {
     const img = require('../../assets/logo-color.png');
     return (
         <VStack bg="primary.200" p={10} minWidth="100%" flex={1}>
-            <Box rounded="md " flex={1} mb={2} justifyContent={"space-around"}>
+            <Box rounded="md" flex={3} mb={2} p={4} justifyContent={"space-between"}>
                 <Center>
                     <Image size={300} source={img} alt="logo"/>
-                </Center>;
-                <HStack justifyContent="space-around">
-                    <Image width="100%"/>
-                </HStack>
-                <HStack justifyContent="space-around">
-                    <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}}
-                            onPress={() => console.log("hello world")}><Text
-                        color="primary.200">Level</Text></Button>
-                    <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}}
-                            onPress={() => console.log("hello world")}><Text
-                        color="primary.200">Total Score</Text></Button>
-                </HStack>
+                </Center>
+
             </Box>
-            <VStack flex={1} mt={3} mb={3} rounded="md" justifyContent="space-between" alignItems="center">
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
-                    color="primary.200">Score</Text></Button>
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
-                    color="primary.200">Resume</Text></Button>
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
-                    color="primary.200">New Game</Text></Button>
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
-                    color="primary.500">Multiplayer</Text></Button>
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
-                    color="primary.200">Leaderboards</Text></Button>
+            <VStack flex={4} mt={4} mb={3} justifyContent="flex-start" alignItems="center">
+                <Button bg="primary.50" mb={5} minWidth={150} _pressed={{backgroundColor: "#fff"}}
+                        onPress={() => console.log("hello world")}><Text fontSize="lg"
+                                                                         color="primary.200">Resume</Text></Button>
+
+                <Button bg="primary.50" mb={5} minWidth={150} size={"lg"} _pressed={{backgroundColor: "#fff"}}
+                        onPress={() => console.log("hello world")}><Text fontSize="lg"
+                                                                         color="primary.200">New Game</Text></Button>
+                <Button bg="muted.900" mb={5} minWidth={150} _pressed={{backgroundColor: "#000"}}
+                        onPress={() => console.log("hello world")}><Text fontSize="lg"
+                                                                         color="muted.50">Multiplayer</Text></Button>
+                <Button bg="primary.50" mb={4} minWidth={150} _pressed={{backgroundColor: "#fff"}}
+                        onPress={() => console.log("hello world")}><Text fontSize="lg"
+                                                                         color="primary.200">Leaderboards</Text></Button>
             </VStack>
             <HStack justifyContent="space-around" alignItems="center">
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
+                <Button bg="primary.50" mr={3} minWidth={100} _pressed={{backgroundColor: "#fff"}}
+                        onPress={() => console.log("hello world")}><Text
                     color="primary.200">Rate Us!</Text></Button>
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
+                <Button bg="primary.50" mr={3} minWidth={100} _pressed={{backgroundColor: "#fff"}}
+                        onPress={() => console.log("hello world")}><Text
                     color="primary.200">How to play</Text></Button>
-                <Button bg="primary.50" _pressed={{backgroundColor: "#fff"}} onPress={() => console.log("hello world")}><Text
+                <Button bg="primary.50" minWidth={100} _pressed={{backgroundColor: "#fff"}}
+                        onPress={() => console.log("hello world")}><Text
                     color="primary.200">Remove Ads!</Text></Button>
             </HStack>
         </VStack>);
